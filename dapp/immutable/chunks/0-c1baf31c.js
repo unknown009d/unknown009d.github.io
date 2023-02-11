4f0ca7e95f07f1f86aa00fe83387d4cc8ff70c92
unknown009d <vbunitynet@gmail.com>
2023-02-11 19:10:24 +0530

    LoadingPageFix

--- dapp/immutable/chunks/0-c1baf31c.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-593668f4.js";export{t as component};
---
+++ dapp/immutable/chunks/0-d32bf9fd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-61d737a3.js";export{t as component};
