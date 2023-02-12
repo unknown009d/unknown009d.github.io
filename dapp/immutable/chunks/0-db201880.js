09d13db7b0baf395dfd949b46e3b1091c7465856
unknown009d <vbunitynet@gmail.com>
2023-02-12 20:06:19 +0530

    OG Optimized

---
+++ dapp/immutable/chunks/0-db201880.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-49b96d10.js";export{t as component};
---
+++ dapp/immutable/chunks/4-0c4b9fe4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/myresume/_page.svelte-4f224190.js";export{t as component};
