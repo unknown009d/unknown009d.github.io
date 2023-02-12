2d85b7959154e172ff3a7bec2b38e2f8e092bf14
unknown009d <vbunitynet@gmail.com>
2023-02-12 18:35:32 +0530

    OG:Added

---
+++ dapp/immutable/chunks/3-222ca9a4.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/aboutme/_page.svelte-95675008.js";export{t as component};
---
+++ dapp/immutable/chunks/4-ca68a5dd.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/myresume/_page.svelte-059c771f.js";export{t as component};
